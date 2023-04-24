a56dcdde2616254a33de31fc828c5a29df8347f2
Eder Daniel Domingues <eder.infonet@gmail.com>
2023-04-24 16:17:51 -0400

    Create table users and migrations

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -1,3 +1,9 @@
+/**
+ * Criação e conexão com banco de dados, vai gerar database.db vazio na 
+ * pasta database/sqlite/database.db, para visualizar o banco de dados
+ * utilizamos o BEEKEEPER instalado no projeto
+*/
+
 const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
 const path = require("path");
