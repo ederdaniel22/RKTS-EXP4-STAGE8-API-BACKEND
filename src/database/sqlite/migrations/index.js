09ed50cf7e32d1fa35ef5013c4c5c60f669053b3
Eder Daniel Domingues <eder.infonet@gmail.com>
2023-06-13 16:31:03 -0400

    Updatinge database and creating Controllers and Routes

--- src/database/sqlite/migrations/index.js
+++ src/database/sqlite/migrations/index.js
@@ -1,15 +1,12 @@
-const sqliteConnection = require('../../sqlite')
-const createUsers = require('./createUsers');
+const sqliteConnection = require("../../sqlite")
+const createUsers = require("./createUsers")
 
-
-async function migrationsRun(){
-  const schemas = [
-    createUsers
-  ].join('');
+async function migrationsRun() {
+  const schemas = [createUsers].join("")
 
   sqliteConnection()
-  .then(db => db.exec(schemas))
-  .catch(error => console.log(error));
+    .then((db) => db.exec(schemas))
+    .catch((error) => console.log(error))
 }
 
-module.exports = migrationsRun;
+module.exports = migrationsRun
