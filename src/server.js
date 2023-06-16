877dd4dfd5f75bd92aea7d7324c16ad2be0bc57c
Eder Daniel Domingues <eder.infonet@gmail.com>
2023-06-16 18:55:56 -0400

    Update and create UserAvatarController

--- src/server.js
+++ src/server.js
@@ -1,37 +1,36 @@
-require("express-async-errors");
-const migrationsRun = require("./database/sqlite/migrations");
+require("express-async-errors")
+const migrationsRun = require("./database/sqlite/migrations")
+const uploadConfig = require("./configs/upload")
 
-const AppError = require("./utils/AppError");
+const AppError = require("./utils/AppError")
 
-const express = require ("express");
-
+const express = require("express")
 
 const routes = require("./routes")
-migrationsRun();
+migrationsRun()
 
+const app = express()
+app.use(express.json())
 
-const app = express();
-app.use(express.json());
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
-app.use(routes);
+app.use(routes)
 
-app.use((error, request, response, next ) => {
-  if(error instanceof AppError){
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
-      message: error.message
-    });
+      message: error.message,
+    })
   }
 
-  console.error(error);
+  console.error(error)
 
   return response.status(500).json({
     status: "error",
     message: "Internal server error",
   })
-
 })
 
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
